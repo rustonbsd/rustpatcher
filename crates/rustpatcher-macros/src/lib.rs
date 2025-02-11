b1bd7231cf41b9a1dbcae018c242cafd080266b0
rustonbsd <rustonbsd@mailfence.com>
2025-02-11 15:06:33 +0100

    sync

--- crates/rustpatcher-macros/src/lib.rs
+++ crates/rustpatcher-macros/src/lib.rs
@@ -5,13 +5,11 @@ use syn::{parse_macro_input, ItemFn};
 #[proc_macro_attribute]
 pub fn main(_args: TokenStream, input: TokenStream) -> TokenStream {
     let input_fn = parse_macro_input!(input as ItemFn);
-    let version = env!("CARGO_PKG_VERSION");
-    
     let expanded = quote! {
         // Create a static initializer that runs before main
         #[ctor::ctor]
         fn __init_version() {
-            rustpatcher::version_embed::__set_version(#version);
+            rustpatcher::version_embed::__set_version(env!("CARGO_PKG_VERSION"));
         }
 
         #input_fn
