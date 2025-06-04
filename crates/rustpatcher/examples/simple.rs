6cf5acfc6471a8e662ea47a6f3a08686df5299cd
rustonbsd <rustonbsd@mailfence.com>
2025-06-04 20:35:32 +0200

    working

--- crates/rustpatcher/examples/simple.rs
+++ crates/rustpatcher/examples/simple.rs
@@ -19,8 +19,8 @@ async fn main() -> anyhow::Result<()> {
     rustpatcher::version_embed::__set_version(Box::leak(v_string.into_boxed_str()));
 
     let patcher = Patcher::new()
-        .trusted_key_from_z32_str(env::var("TRUSTED_KEY")?.as_str())
-        .shared_secret_key_from_z32_str(env::var("SHARED_SECRET_KEY")?.as_str())
+        .trusted_key_from_z32_str("36nqmiugqobr5uw4j7mm8xfbfpc8pggpxnmw6k9sj7x7mtgbdr9o")
+        .shared_secret_key_from_z32_str("3j86j9r7zn1r71xj4ky4nakwhpu1syywrwn9m6ahe5iqp897up1o")
         .update_interval(Duration::from_secs(10))
         .build()
         .await?;
